bf8c949629693404662957b1aec3bf665585dda6
Segun228 <segunperkele@gmail.com>
2025-05-05 15:37:29 +0300

    adding pop and clear methods

--- data structures/trees/b-tree/b4tree.cpp
+++ data structures/trees/b-tree/b4tree.cpp
@@ -162,6 +162,95 @@ class BTree{
             return nullptr;
         }
 
+        void recursive_destroy(Node* target){
+            if(target->children.size()==0){
+                delete target;
+            }
+            else{
+                for(auto child : target->children){
+                    recursive_destroy(child);
+                }
+                delete target;
+            }
+        }
+
+        void fix_underflow(Node* target) {
+            Node* parent = target->parent;
+            if(!parent){
+                return;
+            } 
+            int idx = -1;
+            for (int i = 0; i<parent->children.size(); i++) {
+                if(parent->children[i] == target) {
+                    idx = i;
+                    break;
+                }
+            }
+            Node* left_sibling = (idx > 0) ? parent->children[idx - 1] : nullptr;
+            Node* right_sibling = (idx < parent->children.size() - 1) ? parent->children[idx + 1] : nullptr;
+            if (left_sibling && left_sibling->values.size() > min_keys) {
+                target->values.insert(target->values.begin(), parent->values[idx - 1]);
+                parent->values[idx - 1] = left_sibling->values.back();
+                left_sibling->values.pop_back();
+                if (!left_sibling->children.empty()) {
+                    target->children.insert(target->children.begin(), left_sibling->children.back());
+                    left_sibling->children.back()->parent = target;
+                    left_sibling->children.pop_back();
+                }
+                return;
+            }
+            if (right_sibling && right_sibling->values.size() > min_keys) {
+                target->values.push_back(parent->values[idx]);
+                parent->values[idx] = right_sibling->values.front();
+                right_sibling->values.erase(right_sibling->values.begin());
+        
+                if (!right_sibling->children.empty()) {
+                    target->children.push_back(right_sibling->children.front());
+                    right_sibling->children.front()->parent = target;
+                    right_sibling->children.erase(right_sibling->children.begin());
+                }
+                return;
+            }
+            if(left_sibling){
+                left_sibling->values.push_back(parent->values[idx - 1]);
+                left_sibling->values.insert(left_sibling->values.end(), target->values.begin(), target->values.end());
+                left_sibling->children.insert(left_sibling->children.end(), target->children.begin(), target->children.end());
+                for(auto child : target->children){
+                    if(child){
+                        child->parent = left_sibling;
+                    }
+                }
+                parent->values.erase(parent->values.begin() + idx - 1);
+                parent->children.erase(parent->children.begin() + idx);
+                delete target;
+                if (parent->values.size() < min_keys)
+                    fix_underflow(parent);
+            } 
+            else if(right_sibling){
+                target->values.push_back(parent->values[idx]);
+                target->values.insert(target->values.end(), right_sibling->values.begin(), right_sibling->values.end());
+                target->children.insert(target->children.end(), right_sibling->children.begin(), right_sibling->children.end());
+                for (auto child : right_sibling->children) {
+                    if (child) child->parent = target;
+                }
+                parent->values.erase(parent->values.begin() + idx);
+                parent->children.erase(parent->children.begin() + idx + 1);
+                delete right_sibling;
+                if (parent->values.size() < min_keys)
+                    fix_underflow(parent);
+            }
+            if (parent == root && parent->values.empty()) {
+                if(!parent->children.empty()) {
+                    root = parent->children[0];
+                    root->parent = nullptr;
+                } 
+                else{
+                    root = nullptr;
+                }
+                delete parent;
+            }
+        }
+
     public:
         int t;
         int min_keys, max_keys, min_children, max_children;
@@ -178,7 +267,7 @@ class BTree{
         }
 
         ~BTree(){
-
+            this->clean();
         }
 
         Node* find(int val){
@@ -232,49 +321,56 @@ class BTree{
             return current;
         }
 
-        Node* pop(int val){
-            if(root == nullptr){
-                cout << "ERROR" << endl;
-                cout <<val <<" cannot be popped, the tree is empty"<< endl;
+        Node* pop(int val) {
+            if (root == nullptr) {
+                cout << "ERROR: " << val << " cannot be popped, the tree is empty" << endl;
+                return nullptr;
             }
-            else if(!find(val)){
-                cout << "ERROR" << endl;
-                cout <<val <<" cannot be popped, it is not present in the tree"<< endl;
+            Node* node_with_val = find(val);
+            if (!node_with_val) {
+                cout << "ERROR: " << val << " cannot be popped, it is not present in the tree" << endl;
+                return nullptr;
             }
-            Node* current = root;
-            while(current->children.size()!=0){
-                if(check_node(val, current)){
+            if (node_with_val->children.empty()) {
+                dumb_delete(node_with_val, val);
+                if (node_with_val == root && root->values.empty()) {
+                    delete root;
+                    root = nullptr;
+                    return nullptr;
+                }
+                if (node_with_val->values.size() < min_keys && node_with_val != root) {
+                    fix_underflow(node_with_val);
+                }
+                return root;
+            }
+            int idx = -1;
+            for (int i = 0; i < node_with_val->values.size(); i++) {
+                if (node_with_val->values[i] == val) {
+                    idx = i;
                     break;
                 }
-                bool flag = false;
-                if(val < current->values[0]){
-                    current = current->children[0];
-                    flag = true;
-                }
-                else{
-                    for(int i=1; i<current->values.size(); i++){
-                        if(val > current->values[i-1] && val < current->values[i]){
-                            current = current->children[i];
-                            flag = true;
-                        }
-                    }
-                }
-                if(!flag){
-                    current = current->children.back();
-                }
             }
-            if(current->children.size() == 0){
-
+            if (idx != -1) {
+                Node* pred = node_with_val->children[idx];
+                while (!pred->children.empty())
+                    pred = pred->children.back();
+                int pred_val = pred->values.back();
+                node_with_val->values[idx] = pred_val;
+                dumb_delete(pred, pred_val);
+                if (pred->values.size() < min_keys)
+                    fix_underflow(pred);
             }
-
-
-
-            
-
-
+            return root;
         }
 
         void clean(){
-
+            if(root==nullptr){
+                cout << "The tree was succesfully cleaned" << endl;
+                return;
+            }
+            recursive_destroy(root);
+            root = nullptr;
+            cout << "The tree was succesfully cleaned" << endl;
+            return;
         }
 };
